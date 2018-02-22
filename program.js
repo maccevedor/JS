095725789f2567796674470a7e068de3cde456d8
Mauricio acevedo <maccevedor@gmail.com>
2018-02-22 13:53:07 -0500

    Build Web App

--- program.js
+++ program.js
@@ -1,10 +1,35 @@
+const http = require('http')
+const url = require('url')
 
-var lslib = require('./lslib');
-
-var dirname = process.argv[2];
-var ext = process.argv[3];
-
-lslib(dirname, ext, function(err, files) {
-  for (i = 0; i < files.length; i++) {
-    console.log(files[i]);
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
   }
+}
+
+function unixtime (time) {
+  return { unixtime: time.getTime() }
+}
+
+const server = http.createServer(function (req, res) {
+  const parsedUrl = url.parse(req.url, true)
+  const time = new Date(parsedUrl.query.iso)
+  let result
+
+  if (/^\/api\/parsetime/.test(req.url)) {
+    result = parsetime(time)
+  } else if (/^\/api\/unixtime/.test(req.url)) {
+    result = unixtime(time)
+  }
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(result))
+  } else {
+    res.writeHead(404)
+    res.end()
+  }
+})
+server.listen(Number(process.argv[2]))
